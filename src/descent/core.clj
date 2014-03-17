b5997c8f95010cce8d61c492105ea6ad6148335b
Kenneth Graham <klgraham@gmail.com>
2014-03-17 09:37:30 -0500

    Added a basic main that (for now) just prints the dependencies

--- src/descent/core.clj
+++ src/descent/core.clj
@@ -1,7 +1,14 @@
 (ns descent.core
+  (:require [descent.pom-parser :as parser])
+  (:use [clojure.pprint :only (pprint)])
   (:gen-class))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Given a pom.xml path, this will extract the project's dependencies, load
+  them into Datomic (so they're viewable over time), generate a graph
+  visualization and then print the graph to a file.
+
+  Usage: descent.core: <path-to-pom> [optional arguments]"
+  [path-to-pom & args]
+  (let [deps-map (parser/process-pom path-to-pom)]
+    (pprint deps-map)))
