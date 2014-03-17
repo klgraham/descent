e2ead28c953340cbda0d5f8794d04e319246a9ab
Ken Graham <klgraham@gmail.com>
2014-03-16 23:19:33 -0500

    Organizing code a little better

--- src/descent/pom_parser.clj
+++ src/descent/pom_parser.clj
@@ -4,7 +4,7 @@
 
 (defn load-pom [pom-location] (parse (java.io.File. pom-location)))
 
-;(def pom (load-pom "resources/pom.xml"))
+(def pom (load-pom "resources/pom.xml"))
 
 ;(:tag pom)
 ;(:attrs pom)
@@ -25,15 +25,63 @@
   [pom]
   (-> pom :content (nth 5) :content first))
 
-(defn- get-properties-from-pom
-  "Given a pom, extract the properties and see if they contain dependency version data."
-  [pom]
-  (-> pom :content (nth 7) :content))
-
 ;(get-properties-from-pom pom)
 
 ;(def properties (get-properties-from-pom pom))
 ;(first properties)
+
+
+
+;(contains-version? {:tag :apple.hh})
+;(contains-version? {:tag :rivulet.version})
+
+;(filter-out-properties-without-version properties)
+
+
+;;;; Access the project dependencies through the following logic:
+;;;;   1. if properties section exists, place versions in a hash-map
+;;;;   2. if dependencyManagement section exists, extract deps if it does
+;;;;      if dependencyManagement is absent, just process dependencies instead
+;;;;   3. extract version nmumbers from dependencies and if needed use the properties map
+
+;;; Functions to check if certain pom sections exist
+
+(defn- tag-equals? [m tag] (= (:tag m) tag))
+
+;(get-in pom [:content])
+
+(defn- pom-has-properties?
+  [pom]
+  (some? (filter #(tag-equals? % :properties) (:content pom))))
+
+
+(defn- pom-has-dependencyManagement?
+  [pom]
+  (some? (filter #(tag-equals? % :dependencyManagement) (:content pom))))
+
+
+(defn- pom-has-dependencies?
+  [pom]
+  (some? (filter #(tag-equals? % :dependencies) (:content pom))))
+
+
+;;; Functions to process the properties section
+
+(defn- filter-out-properties-without-version
+  "Returns only those properties that are a version number."
+  [coll]
+  (filter contains-version? coll))
+
+
+(defn- get-properties-from-pom
+  "Given a pom, extract the properties and see if they contain dependency version data."
+  [pom]
+  (-> (filter #(tag-equals? % :properties) (:content pom))
+      first
+      :content
+      filter-out-properties-without-version))
+
+;(get-properties-from-pom pom)
 
 (defn- contains-version?
   "Checks a given member of the pom properties to see if it contains version number."
@@ -45,36 +93,25 @@
                          (= "version"))]
     has-version?))
 
-;(contains-version? {:tag :apple.hh})
-;(contains-version? {:tag :rivulet.version})
 
-(defn- filter-out-properties-without-version
-  "Returns only those properties that are a version number."
-  [coll]
-  (filter contains-version? coll))
-
-;(filter-out-properties-without-version properties)
+(defn- parse-name
+  [name]
+  (-> (clojure.string/split (str name) #"\.")
+      first
+      (subs 1)))
 
 
 (defn- get-dep-version-map
   "Given the properties portion of a pom, return a seq of "
   [properties]
-  (let [version-props (filter-out-properties-without-version properties)
-        parse-name (fn [k](-> (clojure.string/split (str k) #"\.")
-                              first
-                              (subs 1)))]
-    (-> (fn [x] [(parse-name (:tag x)) (-> x :content first)])
-        (map version-props))))
+  (let [properties-map (transient {})]
+    (doseq [p properties]
+      (assoc! properties-map (keyword (parse-name (:tag p))) (first (:content p))))
+    (persistent! properties-map)))
 
-;(get-dep-version-map properties)
+(get-dep-version-map (get-properties-from-pom pom))
 
 
-(defn get-deps-and-versions
-  [pom]
-  (-> (get-properties-from-pom pom)
-      get-dep-version-map))
+;;; Functions to process dependencyManagement
 
-;(get-deps-and-versions pom)
-
-
-
+;;; Functions to process dependencies
